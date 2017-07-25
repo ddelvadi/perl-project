b6271275e442dd3ccde7b33ee010e669b917b175
ddelvadi <ddelvadi@hawk.iit.edu>
2017-07-25 17:43:52 -0500

    Addded perl script to initializa database and defined table in it

---
+++ create-database.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+use DBI;
+use strict;
+
+my $driver   = "SQLite";
+my $database = "information.db";
+my $dsn = "DBI:$driver:dbname=$database";
+my $username = "";
+my $pass = "";
+my $dbh = DBI->connect($dsn, $username, $pass, { RaiseError => 1 })
+   or die $DBI::errstr;
+print "Opened database successfully\n";
+
+my $query = qq(CREATE TABLE USER_INFORMATION
+   (ID INT PRIMARY KEY     NOT NULL,
+      FIRST_NAME           TEXT    NOT NULL,
+      LAST_NAME            TEXT    NOT NULL,
+      HOME                 TEXT ););
+
+my $row = $dbh->do($query);
+if($row < 0) {
+   print $DBI::errstr;
+} else {
+   print "Table in Database Was created successfully\n";
+}
+$dbh->disconnect();
