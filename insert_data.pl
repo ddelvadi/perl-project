621226a2e3d57ffd8f6af5537e1f3f96f939018b
ddelvadi <ddelvadi@hawk.iit.edu>
2017-07-25 18:05:32 -0500

    Added data in perl script to add into USER_INFORMATION table

---
+++ insert_data.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use DBI;
+use strict;
+
+my $driver   = "SQLite";
+my $database = "information.db";
+my $dsn = "DBI:$driver:dbname=$database";
+my $username = "";
+my $pass = "";
+my $dbh = DBI->connect($dsn, $username, $pass, { RaiseError => 1 })
+   or die $DBI::errstr;
+print "Opened database successfully\n";
+
+my $query = qq(INSERT INTO USER_INFORMATION (ID, FIRST_NAME, LAST_NAME, HOME) VALUES (1, 'Rose', 'Tyler', 'Earth'));
+my $row = $dbh->do($query) or die $DBI::errstr;
+
+$query = qq(INSERT INTO USER_INFORMATION (ID, FIRST_NAME, LAST_NAME, HOME) VALUES (2, 'Dipen', 'Delvadiya', 'Earth'));
+$row = $dbh->do($query) or die $DBI::errstr;
+
+print "Data was inserted successfully\n";
+$dbh->disconnect();
